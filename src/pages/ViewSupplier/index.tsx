2b9a1a81a239d8a1f3c609005142cd522748fc78
Maria Emilia Castro <maria.castro@pipefy.com>
2024-09-26 15:33:06 -0300

    feat: add view supplier page

--- src/pages/ViewSupplier/index.tsx
+++ src/pages/ViewSupplier/index.tsx
@@ -1,27 +1,108 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { MdDelete, MdEdit } from "react-icons/md";
+import { useNavigate, useParams } from "react-router-dom";
+import Swal from "sweetalert2";
+import Button from "../../components/Button";
+import useDeleteSupplier from "../../hooks/useDeleteSupplier";
 import useGetSingleSupplier from "../../hooks/useGetSingleSupplier";
 import { Supplier } from "../../types";
-import { ViewSupplierWrapper } from "./styles";
+import {
+  ButtonsWrapper,
+  FieldName,
+  FieldsWrapper,
+  FieldValue,
+  ViewSupplierWrapper,
+} from "./styles";
 
 const ViewSupplier = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
   const { getSingleSupplier } = useGetSingleSupplier();
+  const { deleteSupplier } = useDeleteSupplier();
   const [supplier, setSupplier] = useState<Supplier | null>(null);
 
   useEffect(() => {
     if (id) {
       getSingleSupplier(id).then((data) => {
         setSupplier(data);
-        console.log(data);
       });
     }
   }, []);
 
+  const handleDelete = (id: number) => {
+    Swal.fire({
+      title: "Tem certeza que deseja deletar o fornecedor?",
+      text: "Esta ação é irreversível",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#0082F5",
+      cancelButtonColor: "#B70909",
+      confirmButtonText: "Deletar",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteSupplier(id).then(() => {
+          Swal.fire("Deletado!", "O fornecedor foi deletado.", "success");
+          navigate("/");
+        });
+      }
+    });
+  };
+
+  const fields = [
+    { label: "Nome", key: "name" },
+    { label: "Descrição", key: "description" },
+    { label: "Endereço", key: "address" },
+    { label: "Número", key: "addressNumber" },
+    { label: "Cidade", key: "addressCity" },
+    { label: "Estado", key: "addressState" },
+    { label: "CEP", key: "addressCep" },
+    { label: "Referência", key: "addressReference" },
+    { label: "Contatos", key: "contacts" },
+  ];
+
   return (
     <ViewSupplierWrapper>
       <h1>Detalhes do fornecedor</h1>
-      <p>Nome: {supplier?.name}</p>
+      <FieldsWrapper>
+        {supplier &&
+          fields.map((field) => (
+            <div>
+              <FieldName>{field.label}: </FieldName>
+              <FieldValue>
+                {field.key === "contacts"
+                  ? supplier.contacts.map((contact, index) => (
+                      <div key={index}>
+                        <p>
+                          {contact.name} - {contact.number}
+                        </p>
+                      </div>
+                    ))
+                  : supplier[field.key]}
+              </FieldValue>
+            </div>
+          ))}
+      </FieldsWrapper>
+      <ButtonsWrapper>
+        <Button
+          icon={<MdEdit />}
+          variant="primary"
+          text="Editar"
+          onClick={() => {
+            navigate(`/supplier/${supplier!.id}`);
+          }}
+        />
+        <Button
+          icon={<MdDelete />}
+          variant="delete"
+          text="Deletar"
+          onClick={() => {
+            if (supplier) {
+              handleDelete(supplier.id!);
+            }
+          }}
+        />
+      </ButtonsWrapper>
     </ViewSupplierWrapper>
   );
 };
--- src/pages/ViewSupplier/styles.ts
+++ src/pages/ViewSupplier/styles.ts
@@ -3,6 +3,42 @@ import styled from "styled-components";
 export const ViewSupplierWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 32px;
+  justify-content: start;
+  align-items: start;
+  height: 100%;
+  width: 100%;
+  background-color: #f9f9f9;
+  padding: 24px;
   margin-left: 250px;
+
+  h1 {
+    font-weight: bold;
+    font-size: 48px;
+    color: #666666;
+    margin-bottom: 32px;
+  }
+`;
+
+export const FieldsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  margin-bottom: 32px;
+`;
+
+export const FieldName = styled.p`
+  font-weight: bold;
+  font-size: 16px;
+  color: #666666;
+`;
+
+export const FieldValue = styled.p`
+  font-size: 14px;
+  color: #666666;
+`;
+
+export const ButtonsWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
 `;
